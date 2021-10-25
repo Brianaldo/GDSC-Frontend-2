4312d8e35898d51252bcf1d02397d274936935de
Brianaldo <brianaldophandiarta31@gmail.com>
2021-10-25 11:12:15 +0700

    add modal

--- js/main.js
+++ js/main.js
@@ -1,13 +1,13 @@
 /* *** KAMUS *** */
 const movieList = [
-  { title: "Money Heist1", img: "../img/1.jpg", desc: "" },
-  { title: "Money Heist2", img: "../img/1.jpg", desc: "" },
-  { title: "Money Heist", img: "../img/1.jpg", desc: "" },
-  { title: "Money Heist", img: "../img/1.jpg", desc: "" },
-  { title: "Money Heist", img: "../img/1.jpg", desc: "" },
-  { title: "Money Heist", img: "../img/1.jpg", desc: "" },
-  { title: "Money Heist", img: "../img/1.jpg", desc: "" },
-  { title: "Money Heist", img: "../img/1.jpg", desc: "" },
+  { title: "Money Heist1", img: "../img/1.jpg", desc: "a" },
+  { title: "Money Heist2", img: "../img/1.jpg", desc: "b" },
+  { title: "Money Heist", img: "../img/1.jpg", desc: "c" },
+  { title: "Money Heist", img: "../img/1.jpg", desc: "d" },
+  { title: "Money Heist", img: "../img/1.jpg", desc: "e" },
+  { title: "Money Heist", img: "../img/1.jpg", desc: "f" },
+  { title: "Money Heist", img: "../img/1.jpg", desc: "g" },
+  { title: "Money Heist", img: "../img/1.jpg", desc: "h" },
 ];
 
 /* *** FUNGSI ANTARA *** */
@@ -26,16 +26,36 @@ const buildMovieList = (movieList) => {
     //                 <label>${movieList[i].title}</label>
     //             </div>`;
     newMovie = `<div class="content-movieList-grid-item">
-                    <img class="lazy" src="${movieList[i].img}" />
+                    <img src="${movieList[i].img}" onClick="displayModal('${movieList[i].title}', '${movieList[i].img}', '${movieList[i].desc}')"/>
                     <label>${movieList[i].title}</label>
                 </div>`;
     movieListHTML.innerHTML += newMovie;
   }
 };
 
+const displayModal = (title, img, desc) => {
+  /* KAMUS LOKAL */
+  let modalHTML = document.getElementById("modal")
+  let newModal
+  /* ALGORITMA */
+  modalHTML.style.display = "block";
+  modalHTML.innerHTML = "";
+  newModal = `<div class="modal-content">
+                <div class="modal-content-img">
+                  <img src="${img}"/>
+                </div>
+                <div class="modal-content-span">
+                  <h1>${title}</h1>
+                  <p>${desc}</p>
+                </div>
+              </div>`
+  modalHTML.innerHTML += newModal;
+};
+
 /* *** ALGORITMA *** */
 buildMovieList(movieList);
 
+/* FILTER */
 document
   .getElementById("inputSearch")
   .addEventListener("keyup", (eventInput) => {
@@ -55,6 +75,7 @@ document
     buildMovieList(newMovieList);
   });
 
+/* NAVBAR ANIMATION */
 window.onscroll = () => {
   if (document.body.scrollTop > 25 || document.documentElement.scrollTop > 25) {
     document.getElementById("title").style.fontSize = "30px";
@@ -75,32 +96,39 @@ window.onscroll = () => {
   }
 };
 
-document.addEventListener("DOMContentLoaded", function() {
-  var lazyloadImages = document.querySelectorAll("img.lazy");    
-  var lazyloadThrottleTimeout;
-  
-  function lazyload () {
-    if(lazyloadThrottleTimeout) {
-      clearTimeout(lazyloadThrottleTimeout);
-    }    
-    
-    lazyloadThrottleTimeout = setTimeout(function() {
-        var scrollTop = window.pageYOffset;
-        lazyloadImages.forEach(function(img) {
-            if(img.offsetTop < (window.innerHeight + scrollTop)) {
-              img.src = img.dataset.src;
-              img.classList.remove('lazy');
-            }
-        });
-        if(lazyloadImages.length == 0) { 
-          document.removeEventListener("scroll", lazyload);
-          window.removeEventListener("resize", lazyload);
-          window.removeEventListener("orientationChange", lazyload);
-        }
-    }, 20);
-  }
-  
-  document.addEventListener("scroll", lazyload);
-  window.addEventListener("resize", lazyload);
-  window.addEventListener("orientationChange", lazyload);
-});
+/* LAZY LOAD */
+// document.addEventListener("DOMContentLoaded", function () {
+//   var lazyloadImages = document.querySelectorAll("img.lazy");
+//   var lazyloadThrottleTimeout;
+
+//   function lazyload() {
+//     if (lazyloadThrottleTimeout) {
+//       clearTimeout(lazyloadThrottleTimeout);
+//     }
+
+//     lazyloadThrottleTimeout = setTimeout(function () {
+//       var scrollTop = window.pageYOffset;
+//       lazyloadImages.forEach(function (img) {
+//         if (img.offsetTop < window.innerHeight + scrollTop) {
+//           img.src = img.dataset.src;
+//           img.classList.remove("lazy");
+//         }
+//       });
+//       if (lazyloadImages.length == 0) {
+//         document.removeEventListener("scroll", lazyload);
+//         window.removeEventListener("resize", lazyload);
+//         window.removeEventListener("orientationChange", lazyload);
+//       }
+//     }, 20);
+//   }
+//
+//   document.addEventListener("scroll", lazyload);
+//   window.addEventListener("resize", lazyload);
+//   window.addEventListener("orientationChange", lazyload);
+// });
+
+/* MODAL */
+window.onclick = (eventModal) => {
+  if (eventModal.target === document.getElementById("modal"))
+    document.getElementById("modal").style.display = "none";
+};
