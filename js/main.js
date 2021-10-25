348762a5631d9e1951c407cf87fa65d8ef9ca34b
Brianaldo <brianaldophandiarta31@gmail.com>
2021-10-25 09:19:41 +0700

    test lazy load

--- js/main.js
+++ js/main.js
@@ -21,8 +21,12 @@ const buildMovieList = (movieList) => {
   movieListHTML = document.getElementById("movieList-grid");
   movieListHTML.innerHTML = "";
   for (i = 0; i < movieList.length; i++) {
+    // newMovie = `<div class="content-movieList-grid-item">
+    //                 <img class="lazy" data-src="${movieList[i].img}" />
+    //                 <label>${movieList[i].title}</label>
+    //             </div>`;
     newMovie = `<div class="content-movieList-grid-item">
-                    <img src="${movieList[i].img}" />
+                    <img class="lazy" src="${movieList[i].img}" />
                     <label>${movieList[i].title}</label>
                 </div>`;
     movieListHTML.innerHTML += newMovie;
@@ -70,3 +74,33 @@ window.onscroll = () => {
     }
   }
 };
+
+document.addEventListener("DOMContentLoaded", function() {
+  var lazyloadImages = document.querySelectorAll("img.lazy");    
+  var lazyloadThrottleTimeout;
+  
+  function lazyload () {
+    if(lazyloadThrottleTimeout) {
+      clearTimeout(lazyloadThrottleTimeout);
+    }    
+    
+    lazyloadThrottleTimeout = setTimeout(function() {
+        var scrollTop = window.pageYOffset;
+        lazyloadImages.forEach(function(img) {
+            if(img.offsetTop < (window.innerHeight + scrollTop)) {
+              img.src = img.dataset.src;
+              img.classList.remove('lazy');
+            }
+        });
+        if(lazyloadImages.length == 0) { 
+          document.removeEventListener("scroll", lazyload);
+          window.removeEventListener("resize", lazyload);
+          window.removeEventListener("orientationChange", lazyload);
+        }
+    }, 20);
+  }
+  
+  document.addEventListener("scroll", lazyload);
+  window.addEventListener("resize", lazyload);
+  window.addEventListener("orientationChange", lazyload);
+});
